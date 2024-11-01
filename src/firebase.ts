768849c62e858e61f51de0adb04b1f28e342b026
Tanjil Rahman <tanjil.rahman739@gmail.com>
2024-11-01 22:12:21 +0600

    initial commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,14 @@
+require("dotenv").config();
 import * as admin from "firebase-admin";
-import serviceAccount from "firebase-service-account.json"; // Path to your service account
 
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("Missing FIREBASE_SERVICE_ACCOUNT environment variable.");
+}
+
+// Decode and parse the Base64 JSON string
+const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf-8"));
+
+// Initialize Firebase Admin SDK with the service account credentials
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
